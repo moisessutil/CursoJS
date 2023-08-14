d6d6c8734ad0da69c0601d6704452ac5bd46b10a
Moises Sutil <131698525+moisessutil@users.noreply.github.com>
2023-08-13 22:27:57 -0300

    es6

---
+++ javascript-es6/script.js
@@ -0,0 +1,31 @@
+// var, let e const
+var x = 10
+var y = 15
+
+if (y > 10) {
+    var x = 5
+    console.log(x)
+}
+
+console.log(x)
+
+let a = 10
+let b = 15
+
+if (b > 10) {
+    let a = 5
+    console.log(a)
+}
+
+console.log(a)
+
+function logName() {
+    const name = 'João'
+    console.log(name)
+}
+
+const name = 'Pedro'
+
+logName()
+
+console.log(name)
